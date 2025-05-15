d66487a83916f61ac8caf0ae7cbc18b358e1a601
stephenccodes <stephen.carroll.6@gmail.com>
2025-05-15 13:42:53 -0700

    add sql file

---
+++ eds213_crop_project.sql
@@ -0,0 +1,16 @@
+-- Pull data into tables
+CREATE TABLE crop_calendar AS
+    SELECT * FROM read_csv('data_processed/crop_calendar_clean.csv');
+
+CREATE TABLE crop_yield AS
+    SELECT * FROM read_csv('data_processed/fao_clean.csv');
+
+-- Make an intermediary table
+
+CREATE TABLE yield_calendar AS
+SELECT * 
+FROM crop_yield
+LEFT JOIN crop_calendar
+ON crop_yield.country = crop_calendar.country AND crop_yield.crop = crop_calendar.crop; 
+
+
